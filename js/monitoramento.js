847e66e96ec6824845dd99e96aa2c41b558df071
Igor Oliveira <igoroliveira.oliveira@gmail.com>
2025-07-14 16:31:44 -0300

    Estado inicial

---
+++ js/monitoramento.js
@@ -0,0 +1,17 @@
+$.ajax({
+    /*url: "http://l5d1137.callbox.com.br/monitoramento/lib/ramais.php", */
+    url: "http://localhost:8000/lib/ramais.php",
+    type: "GET",
+    success: function(data){                
+        for(let i in data){
+            $('#cartoes').append(`<div class="cartao">
+                                <div>${data[i].nome}</div>
+                                <span class="${data[i].status} icone-posicao"></span>
+                              </div>`)
+        }
+        
+    },
+    error: function(){
+        console.log("Errouu!")
+    }
+});
