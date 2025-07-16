31fa915b8d2a00953e8a9de3c2995d6720b7bab6
Igor Oliveira <igoroliveira.oliveira@gmail.com>
2025-07-16 16:50:33 -0300

    Feature: atualiza card a cada 10 segundos

--- js/monitoramento.js
+++ js/monitoramento.js
@@ -1,26 +1,42 @@
-$.ajax({
-    url: "http://localhost:8000/lib/ramais-api.php",
+function atualizarCartoes() {
+  $.ajax({
+    url: "http://localhost:8000/lib/atualizar.php",
     type: "GET",
-    success: function(data){
-        for(let i in data){
-            if(data[i].status == 'indisponivel') {
-                $('#cartoes').append(`<div class="cartao cartao-indisponivel">
-                    <div>${data[i].nome}(${data[i].nome_agente})</div>
-                    <span class="${data[i].status} icone-posicao"></span>
-                </div>`)
-            } else {
-                $('#cartoes').append(`<div class="cartao">
-                    <div>${data[i].nome}(${data[i].nome_agente})</div>
-                    <span class="${data[i].status} icone-posicao"></span>
-                </div>`)
-            }
-        }
-    },
-    error: function(jqXHR) {
-        $('#cartoes').html(`<div class="cartao cartao-erro">
-            Erro ao carregar os ramais.
-        </div>`);
+    success: function() {
+      $.ajax({
+        url: "http://localhost:8000/lib/ramais-api.php",
+        type: "GET",
+        success: function(data) {
+          $('#cartoes').empty();
 
-        console.log("Erro AJAX:", jqXHR.responseText);
-    }
-});
+          for (let i in data) {
+            const classeCartao = data[i].status === 'indisponivel' ? 'cartao cartao-indisponivel' : 'cartao';
+            $('#cartoes').append(`
+              <div class="${classeCartao}">
+                <div>${data[i].nome} (${data[i].nome_agente ?? ''})</div>
+                <span class="${data[i].status} icone-posicao"></span>
+              </div>`);
+          }
+        },
+        error: function(jqXHR) {
+            $('#cartoes').html(`<div class="cartao cartao-erro">
+                Erro ao carregar os ramais.
+            </div>`);
+
+            console.log("Erro AJAX:", jqXHR.responseText);
+        }
+      });
+    },
+        error: function(jqXHR) {
+            $('#cartoes').html(`<div class="cartao cartao-erro">
+                Erro ao atualizar banco de dados.
+            </div>`);
+
+            console.log("Erro AJAX:", jqXHR.responseText);
+        }
+  });
+}
+
+atualizarCartoes();
+
+setInterval(atualizarCartoes, 10000);
